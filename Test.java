c00d693f9d35ca0ed3f290d1026dda7afe2a50ef
Kaif Shaikh <kaifshaikh1221@gmail.com>
2023-03-04 05:56:14 -0800

    Comiting for test

---
+++ Test.java
@@ -0,0 +1,10 @@
+class A{
+public static void main(string args[]){
+system.out.println("Hello World");
+}
+}
+class B{
+public static void main(string args[]){
+system.out.println("This is my java_jenkings test");
+}
+}
